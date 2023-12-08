f1a75f0f2b71af7685df689891fd1bf0543435ec
Piyush Saxena <piyush.saxena@globalvoxinc.com>
2023-12-08 12:09:29 +0530

    404 page back button text change

--- app/not-found.js
+++ app/not-found.js
@@ -10,7 +10,7 @@ export default function NotFound() {
         onClick={() => router.push({ pathName: "/" })}
         className="bg-red-300 mt-3 p-2 rounded-full border border-red-700 text-sm capitalize hover:bg-red-700 hover:text-white"
       >
-        Home
+        Back to Home
       </button>
     </div>
   );
